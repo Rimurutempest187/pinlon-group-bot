f4a059a746a662496444db9844763668d92b8c2f
Rimurutempest187 <karangmerry@gmail.com>
2026-02-02 05:30:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS").split(",")]
-
-# File paths
-USERS_FILE = "data/users.json"
-QUIZZES_FILE = "data/quizzes.json"
-EVENTS_FILE = "data/events.json"
-VERSES_FILE = "data/verses.json"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+ADMIN_IDS = [1812962224]  # Telegram IDs of admins
+DEFAULT_LANGUAGE = "en"
