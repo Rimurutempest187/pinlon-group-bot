69012cd430702f632d678ff0abd0f67bfa3efd17
Rimurutempest187 <karangmerry@gmail.com>
2026-02-02 03:48:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS").split(",")]
 
+# File paths
+USERS_FILE = "data/users.json"
+QUIZZES_FILE = "data/quizzes.json"
+EVENTS_FILE = "data/events.json"
+VERSES_FILE = "data/verses.json"
