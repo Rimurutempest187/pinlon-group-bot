2c6859fba28971f2e8ba325b2b4ef2d84cdbe908
Rimurutempest187 <karangmerry@gmail.com>
2026-02-02 02:24:16 +0700

    Create google_calendar.py

---
+++ google_calendar.py
@@ -0,0 +1,22 @@
+# utils/google_calendar.py
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import datetime
+
+def get_upcoming_events():
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    creds = service_account.Credentials.from_service_account_file(
+        './' + config.GOOGLE_CREDENTIALS, scopes=SCOPES
+    )
+    service = build('calendar', 'v3', credentials=creds)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    events_result = service.events().list(
+        calendarId=config.GOOGLE_CALENDAR_ID,
+        timeMin=now,
+        maxResults=10,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+    events = events_result.get('items', [])
+    return events
