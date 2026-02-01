8112861a2a5a5dce2dff834145c355d0b7d756b4
Rimurutempest187 <karangmerry@gmail.com>
2026-02-02 02:09:45 +0700

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -1,1 +1,12 @@
+# utils/broadcast.py
+import json
+
+async def broadcast_message(bot, users_file, message):
+    with open(users_file, "r") as f:
+        users = json.load(f)
+    for uid in users.keys():
+        try:
+            await bot.send_message(chat_id=int(uid), text=f"📢 Announcement:\n{message}")
+        except:
+            continue
 
